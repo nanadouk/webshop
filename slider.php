c48637bdb0ca517a23f0f5c33fd6c25948a33231
nanadouk <anna.doukmak@gmail.com>
2018-01-01 12:41:03 +0100

    Updated header and slider in home view

--- slider.php
+++ slider.php
@@ -1,3 +1,14 @@
-<div id="slider" class="row">
-    <img src="assets/img/pizza.jpg" alt="pizza" />
+<div class="row">
+    <div id="slider-info">
+        <div id="contact-info">
+            <i class="fa fa-phone"></i>
+            <p>032 321 61 11</p>
+            <p>call us</p>
+        </div>
+        <div id="slider-info-2">
+            <p id="slider-info-titel">Enjoy</p>
+            <p>The original Italian Pizza</p>
+        </div>
+    </div>
+    <div id="slider-img"><img src="assets/img/pizza.jpg" alt="pizza" /></div>
 </div>
